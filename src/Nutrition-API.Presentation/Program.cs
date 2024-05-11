b226b5c65f24ef99ada6e27dd88696c9986c4764
Samra Mammadli <memmedlisemra6@gmail.com>
2024-05-11 16:01:12 +0400

    Add blob container

--- src/Nutrition-API.Presentation/Program.cs
+++ src/Nutrition-API.Presentation/Program.cs
@@ -1,44 +1,89 @@
+using Microsoft.OpenApi.Models;
+using Nutrition_API.Infrastructure.Data;
+using Nutrition_API.Infrastructure.Repositories;
+using Nutrition_API.Core.Repositories;
+using Nutrition_API.Presentation.Options;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var blobImageOptionsSection = builder.Configuration.GetSection("BlobImageOptions");
+
+var blobImageOptions = blobImageOptionsSection.Get<BlobOptions>() ?? throw new Exception("Couldn't create blob image options object");
+
+builder.Services.Configure<BlobOptions>(blobImageOptionsSection);
+
+var blobVideoOptionsSection = builder.Configuration.GetSection("BlobVideoOptions");
+
+var blobVideoOptions = blobVideoOptionsSection.Get<BlobOptions>() ?? throw new Exception("Couldn't create blob video options object");
+
+builder.Services.Configure<BlobOptions>(blobVideoOptionsSection);
+
+var infrastructureAssembly = typeof(FoodDbContext).Assembly;
+
+builder.Services.AddMediatR(configurations =>
+{
+    configurations.RegisterServicesFromAssembly(infrastructureAssembly);
+});
+
+builder.Services.AddScoped<IFoodRepository, FoodSqlRepository>();
+
+builder.Services.AddAuthorization();
+
+var connectionString = builder.Configuration.GetConnectionString("FoodDb");
+
+builder.Services.AddDbContext<FoodDbContext>(dbContextOptionsBuilder =>
+{
+    dbContextOptionsBuilder.UseNpgsql(connectionString, o =>
+    {
+        o.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+    });
+});
+
+builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "4FitBody (api for working staff)",
+        Version = "v1"
+    });
+});
+
+builder.Services.AddAuthentication();
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("BlazorWasmPolicy", corsBuilder => {
+        corsBuilder
+            .WithOrigins("http://localhost:5160")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
+
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthentication();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors("BlazorWasmPolicy");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
